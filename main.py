ba4fc1315473545189d02596de0541085f539404
Doomzes <72573533+Doomzes@users.noreply.github.com>
2020-10-10 12:41:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -121,7 +121,6 @@ It looks like we will soon have more rabbits!"""
 
 
 animals = [camel, lion, deer, goose, bat, rabbit]
-# write your code here
 while True:
     habitat = input('Please enter the number of the habitat you would like to view: >')
     if habitat != 'exit':
