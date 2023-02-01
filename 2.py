85c7788c36a53ae7e40c075f7e8935019f32f62c
Doomzes <72573533+Doomzes@users.noreply.github.com>
2023-02-01 14:18:33 +0200

    Arithmetic Exam Application

--- 2.py
+++ 2.py
@@ -1,58 +1,123 @@
-def greet(bot_name, birth_year):
-    print('Hello! My name is ' + bot_name + '.')
-    print('I was created in ' + birth_year + '.')
+import random
+
+def choose_level():
+    global level
+
+    while True:
+        print("Which level do you want? Enter a number:")
+        print("1 - simple operations with numbers 2-9")
+        print("2 - integral squares of 11-29")
+        try:
+            level = int(input())
+            if 1 <= level <= 2:
+                setteings.append(level)
+                break
+            else:
+                print("Incorrect format.")
+        except ValueError:
+            print("Incorrect format.")
 
 
-def remind_name():
-    print('Please, remind me your name.')
+def random_level_1():
+    global equation
+    global math_operation
+    random.shuffle(math_operation)
+    random.randrange(2, 10, 1)
+    equation = random.randrange(2, 10, 1), random.randrange(2, 10, 1)
+
+
+def random_level_2():
+    global squares
+    squares = random.randrange(11, 30, 1)
+
+
+def action_1(answer):
+    global i
+    global point
+    if math_operation[1] == "+":
+        if answer == equation[0] + equation[1]:
+            print("Right!")
+            point += 1
+        else:
+            print("Wrong!")
+    elif math_operation[1] == "-":
+        if answer == equation[0] - equation[1]:
+            print("Right!")
+            point += 1
+        else:
+            print("Wrong!")
+    elif math_operation[1] == "*":
+        if answer == equation[0] * equation[1]:
+            print("Right!")
+            point += 1
+        else:
+            print("Wrong!")
+    i += 1
+
+
+def action_2(answer):
+    global i
+    global point
+    if answer == squares ** 2:
+        print("Right!")
+        point += 1
+    else:
+        print("Wrong!")
+    i += 1
+
+
+def save(name, point, lvl, desc):
+    file = open('results.txt', 'a', encoding='utf-8')
+    file.writelines("{}: {}/5 in level {} ({})".format(name, point, lvl, desc) + '\n')
+    file.close()
+
+
+point = 0
+i = 0
+squares = 0
+equation = []
+setteings = []
+math_operation = ["+", "-", "*"]
+
+
+while True:
+    choose_level()
+    if level == 1:
+        setteings.append("1 - simple operations with numbers 2-9")
+        while i < 5:
+            random_level_1()
+            while True:
+                print("{} {} {}".format(equation[0], math_operation[1], equation[1]))
+                try:
+                    answer = int(input())
+                    action_1(answer)
+                    break
+                except ValueError:
+                    print("Incorrect format.")
+        break
+    else:
+        setteings.append("2 - integral squares of 11-29")
+        while i < 5:
+            random_level_2()
+            while True:
+                print("{}".format(squares))
+                try:
+                    answer = int(input())
+                    action_2(answer)
+                    break
+                except ValueError:
+                    print("Incorrect format.")
+        break
+
+
+
+
+print("Your mark is {}/5. Would you like to save the result? Enter yes or no.".format(point))
+answer_save = input()
+if answer_save in ["Yes","yes","YES","y"]:
+    print("What is your name?")
     name = input()
-    print('What a great name you have, ' + name + '!')
-
-
-def guess_age():
-    print('Let me guess your age.')
-    print('Enter remainders of dividing your age by 3, 5 and 7.')
-
-    rem3 = int(input())
-    rem5 = int(input())
-    rem7 = int(input())
-    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
-
-    print("Your age is " + str(age) + "; that's a good time to start programming!")
-
-
-def count():
-    print('Now I will prove to you that I can count to any number you want.')
-
-    num = int(input())
-    curr = 0
-    while curr <= num:
-        print(curr, '!')
-        curr = curr + 1
-
-
-def test():
-    print("Let's test your programming knowledge.")
-    print("Why do we use methods?")
-    print('''
-    1. To repeat a statement multiple times.
-    2. To decompose a program into several small subroutines.
-    3. To determine the execution time of a program.
-    4. To interrupt the execution of a program.
-    ''')
-    answer = int(input())
-    while answer != 2:
-        print('Please, try again.')
-        answer = int(input())
-
-
-def end():
-    print('Congratulations, have a nice day!')
-
-
-greet('Aid', '2020')  # change it as you need
-remind_name()
-guess_age()
-count()
-test()
-end()
+    save(name, point, setteings[0], setteings[1])
+    print('The results are saved in "results.txt".')
+else:
+    exit()
